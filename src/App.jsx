33633d39dfe6f0979fc81f7ad1f3b28111ad374e
rabiulhasanriaz <riaz@iglhost.com>
2026-06-20 12:43:23 +0200

    Componenet separate

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
-const reactDescriptions = ['Fundamental','Core','Crucial'];
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max + 1));
-}
-function Header(){
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
+
 function Main(){
   return (
     <main>
@@ -34,16 +19,6 @@ function Main(){
   );
 }
 
-
-function CoreConcept(props){
-  return (
-  <li>
-    <img src={props.image} alt={props.title} />
-    <h3>{props.title}</h3>
-    <p>{props.description}</p>
-  </li>
-  );
-}
 
 function App() {
   return (
