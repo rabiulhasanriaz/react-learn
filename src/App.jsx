380eca31266590ea7fdc66280785b8175afc4dd0
rabiulhasanriaz <riaz@iglhost.com>
2026-06-30 10:32:03 +0200

    component separate

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,14 @@
-import { useState, Fragment } from 'react';
-import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-import { EXAMPLES } from './data.js';
-
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
+import { Fragment } from 'react';
 
 function Main(){
-  const [ selectedTopic, setSelectedTopic ] = useState();
-  let tabContent = "Please select a button";
-  if(selectedTopic){
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>
-            {EXAMPLES[selectedTopic].code}
-            </code>
-          </pre>
-        </div> 
-    );
-  }
-
-  function handleSelect(selectedButton){
-    setSelectedTopic(selectedButton);
-    }
+ 
   return (
     <main>
-      <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          {CORE_CONCEPTS.map((conceptItem) => (
-            <CoreConcept key={conceptItem.title} {...conceptItem} />
-          ))}
-        </ul>
-      </section>
-      <section id="examples">
-        <h2>Examples</h2>
-        <menu>
-          <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>Jsx</TabButton>
-          <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-        </menu>
-        {tabContent}
-      </section>
+      <CoreConcepts />
+      <Examples />
     </main>
   );
 }
